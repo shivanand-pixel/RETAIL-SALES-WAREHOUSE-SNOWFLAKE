ddc3b1447c8f623fc61ea70df045a0af32faf68d
shivanand <76648780+shivanand-pixel@users.noreply.github.com>
2026-02-25 09:30:07 +0000

    sql

---
+++ sql/04_scd.sql
@@ -0,0 +1,58 @@
+DESC TABLE SNOWFLAKE_SAMPLE_DATA.TPCDS_SF100TCL.CUSTOMER;
+
+DESC TABLE SNOWFLAKE_SAMPLE_DATA.TPCDS_SF100TCL.CUSTOMER_ADDRESS;
+
+USE WAREHOUSE COMPUTE_WH;
+USE DATABASE SNOWFLAKE_LEARNING_DB;
+CREATE OR REPLACE TABLE SILVER.CUSTOMER_DIM(
+    CUSTOMER_SK NUMBER AUTOINCREMENT,
+    CUSTOMER_ID NUMBER,
+    STATE STRING,
+    START_DATE  DATE,
+    END_DATE DATE,
+    IS_CURRENT STRING
+);
+
+
+INSERT INTO SILVER.CUSTOMER_DIM (CUSTOMER_ID, STATE, START_DATE, END_DATE, IS_CURRENT)
+SELECT
+    c_customer_sk AS CUSTOMER_ID,
+    ca.ca_state AS STATE,
+    CURRENT_DATE AS START_DATE,
+    NULL AS END_DATE,
+    'Y' AS IS_CURRENT   
+FROM SNOWFLAKE_SAMPLE_DATA.TPCDS_SF100TCL.CUSTOMER c
+JOIN SNOWFLAKE_SAMPLE_DATA.TPCDS_SF100TCL.CUSTOMER_ADDRESS ca
+ON c.c_current_addr_sk = ca.ca_address_sk;
+
+select  count(*) from silver.CUSTOMER_DIM; 
+
+
+CREATE OR REPLACE TABLE SILVER.CUSTOMER_STAGE AS
+SELECT
+     CUSTOMER_ID,STATE    
+    FROM SILVER.CUSTOMER_DIM
+    WHERE is_current = 'Y';
+
+    --manually update one row
+UPDATE SILVER.CUSTOMER_DIM
+set STATE = 'NY'
+WHERE CUSTOMER_ID = 31623748;
+
+
+
+
+--implement stage 2 using merge
+MERGE INTO SILVER.CUSTOMER_DIM target
+USING SILVER.CUSTOMER_STAGE source 
+On target.customer_id=source.customer_id
+AND target.is_current='Y'
+WHEN MATCHED AND target.state <> source.state THEN
+UPDATE 
+SET target.end_date = CURRENT_DATE, 
+target.is_current = 'N'
+WHEN NOT MATCHED THEN
+INSERT (customer_id, state, start_date, end_date, is_current)
+VALUES (source.customer_id, source.state, CURRENT_DATE, NULL, 'Y');
+
+select  * from silver.customer_dim where customer_id=31623748 order by start_date;
